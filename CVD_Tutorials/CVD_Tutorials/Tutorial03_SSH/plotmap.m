1fd4741e988cbf9001fd3f318678bb9782a456f2
kfmit <kfung@mit.edu>
2021-09-23 12:49:54 -0400

    why different

--- CVD_Tutorials/CVD_Tutorials/Tutorial03_SSH/plotmap.m
+++
@@ -1,4 +0,0 @@
-function r=plotmap()
-load 'm_coasts.mat';
-%ind=find(ncst(:,1)<0);ncst(ind,1)=ncst(ind,1)+360;ind=find(ncst(:,1)>359);ncst(ind,1)=NaN;
-hold on;plot(ncst(:,1),ncst(:,2),'k','linewidth',1);
